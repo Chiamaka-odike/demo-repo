1aef15ec9af5bab8d259861b629fe48cd5aabad3
Charles <okoyecharles509@gmail.com>
2025-02-27 17:29:16 +0100

    reolve css issues

--- asset/js/script.js
+++ asset/js/script.js
@@ -11,37 +11,29 @@ document.addEventListener("DOMContentLoaded", async function () {
   data.forEach((category) => {
     dropDown.innerHTML += `
         <div class ="itemBox">
-               <a class="categoryItem" href="category.html?slug=${category.slug}">
-    <p class="itemSlug">${category.name}</p>
-</a>
-
+          <a class="categoryItem" href="category.html?slug=${category.slug}">
+            <p class="itemSlug">${category.name}</p>
+          </a>
         </div>
-        `;
+    `;
   });
 
   link.addEventListener("click", function () {
-    console.log("button clicked");
     toggleDropDown.classList.toggle("hide");
     dropDown.classList.toggle("hide");
   });
 
-    const sideBar = document.querySelector(".sideBar")
-    sideBar.addEventListener("click", function (){
-      console.log("clicked")
-    })
-   
-  
- 
-  data.slice(0, 6).forEach((category) => {
+  const sideBar = document.querySelector(".sideBar");
+  // sideBar.addEventListener("click", function () {
+  //   console.log("clicked");
+  // });
+
+
+  data.forEach((category) => {
     categoiesList.innerHTML += `
-       <a class="categoryItem" href="category.html?slug=${category.slug}">
-         
-          <p>${category.name}</p>
-        </a>`;
+      <a class="categoryItem" href="category.html?slug=${category.slug}">
+        <p>${category.name}</p>
+      </a>
+    `;
   });
-
-  
-    
-
-  });
-
+});
